093dc5a767bea18236c3a09fcb3e722370040f0c
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2018-05-04 03:50:16 +0200

    Added doc strings

--- src/GraDrAna/TypeDefs.hs
+++ src/GraDrAna/TypeDefs.hs
@@ -2,16 +2,20 @@ module GraDrAna.TypeDefs where
 
 import qualified Data.Map as Map
 
-type PersonId = String
-
-data Gender = Male | Female | Other
+-- | ADT representing possible genders.
+data Gender = Female | Male | Inter | Other
+  -- FIXME: Add some more!
   deriving (Show)
 
+type PersonId = String
+
+-- | A person or rather role.
 data Person = Person
-  { _person_id :: PersonId
-  , _person_role :: Maybe String
-  , _person_desc :: Maybe String
-  , _person_gender :: Maybe Gender
+  { _person_id :: PersonId          -- ^ the role's identifier
+  , _person_role :: Maybe String    -- ^ the name
+  , _person_desc :: Maybe String    -- ^ the role's description
+  , _person_gender :: Maybe Gender  -- ^ the role's gender
   } deriving (Show)
 
+-- | A map representing the register of persons of a play.
 type Persons = Map.Map PersonId Person
