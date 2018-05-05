30f77e5d84239bb2b7ad154fdf1baf3230ac96bd
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2018-05-06 00:27:45 +0200

    Doc strings and grouping

--- src/GraDrAna/ArrowXml.hs
+++ src/GraDrAna/ArrowXml.hs
@@ -22,43 +22,34 @@ import qualified Text.XML.HXT.DOM.XmlNode as XN
 import Data.Char
 import Data.Maybe
 
+-- * Basic functions and arrows for case insensitiv matching
 
--- * Matching names against a list
-
-nameIn :: (ArrowXml a) => [String] -> a XmlTree XmlTree
-nameIn names = (getName >>> isA (flip elem names)) `guards` this
-{-# INLINE nameIn #-}
-
-qNameIn :: (ArrowXml a) => [QName] -> a XmlTree XmlTree
-qNameIn qNames = (getQName >>> isA (flip elem qNames)) `guards` this
-{-# INLINE qNameIn #-}
-
--- * Sripping elements from the tree.
-
-stripName :: (ArrowXml a) => String -> a XmlTree XmlTree
-stripName n = processTopDown (filterA $ neg (hasName n))
-{-# INLINE stripName #-}
-
--- this does not work, we need neg or ifA to pass nodes through
-stripName' :: (ArrowXml a) => String -> a XmlTree XmlTree
-stripName' n = processTopDown (filterA (getName >>> isA (/= n)) `guards` this)
-
-stripNames :: (ArrowXml a) => [String] -> a XmlTree XmlTree
-stripNames names = processTopDown $ neg $ (nameIn names) `guards` this
-{-# INLINE stripNames #-}
-
-stripQNames :: (ArrowXml a) => [QName] -> a XmlTree XmlTree
-stripQNames qNames = processTopDown $ neg $ (qNameIn qNames) `guards` this
-
--- * Case insensitive arrows
-
+-- | Case convertion for case insensitive matches.
 caseFun :: Char -> Char
 caseFun = toUpper
 {-# INLINE caseFun #-}
 
+-- | Convert the case of a 'QName' using 'caseFun'.
+makeQNameCase :: QName -> QName
+makeQNameCase n = mkQName (map caseFun $ namePrefix n) (map caseFun $ localPart n) (map caseFun $ namespaceUri n)
+{-# INLINE makeQNameCase #-}
+
+-- | Case insensitive variant of hxt's 'hasQName'.
+hasQNameCase :: (ArrowXml a) => QName -> a XmlTree XmlTree
+hasQNameCase n = (getQNameCase >>> isA (== (makeQNameCase n))) `guards` this
+{-# INLINE hasQNameCase #-}
+
+-- | Case insensitive variant of hxt's 'getQName'.
+getQNameCase :: (ArrowXml a) => a XmlTree QName
+getQNameCase = arrL (maybeToList . (fmap makeQNameCase) . XN.getName)
+{-# INLINE getQNameCase #-}
+
+-- * Accessing attributes case insensitively
+
 -- | Select the value of an attribute of an element node. The
 -- attribute name is matched case insensitive. This always succeeds
--- with an empty string as default value.
+-- with an empty string as default value. This is a case insensitive
+-- variant of hxt's 'getAttrValue'.
 getAttrCaseValue :: (ArrowXml a) => String -> a XmlTree String
 getAttrCaseValue n =
   xshow (getAttrl >>>
@@ -69,7 +60,8 @@ getAttrCaseValue n =
     upper = map caseFun
 {-# INLINE getAttrCaseValue #-}
 
--- | Like 'getAttrCaseValue', but fails if the attribute does not exist
+-- | Like 'getAttrCaseValue', but fails if the attribute does not
+-- exist. This is a case insensitive variant of hxt's 'getAttrValue0'.
 getAttrCaseValue0 :: (ArrowXml a) => String -> a XmlTree String
 getAttrCaseValue0 n =
   getAttrl >>>
@@ -80,26 +72,52 @@ getAttrCaseValue0 n =
     upper = map caseFun
 {-# INLINE getAttrCaseValue0 #-}
 
-makeQNameCase :: QName -> QName
-makeQNameCase n = mkQName (map caseFun $ namePrefix n) (map caseFun $ localPart n) (map caseFun $ namespaceUri n)
-{-# INLINE makeQNameCase #-}
+-- * Matching names of elements or attributes against a list
 
-hasQNameCase :: (ArrowXml a) => QName -> a XmlTree XmlTree
-hasQNameCase n = (getQNameCase >>> isA (== (makeQNameCase n))) `guards` this
-{-# INLINE hasQNameCase #-}
+-- | Matching against a list of unqualified names.
+nameIn :: (ArrowXml a) => [String] -> a XmlTree XmlTree
+nameIn names = (getName >>> isA (flip elem names)) `guards` this
+{-# INLINE nameIn #-}
 
-getQNameCase :: (ArrowXml a) => a XmlTree QName
-getQNameCase = arrL (maybeToList . (fmap makeQNameCase) . XN.getName)
-{-# INLINE getQNameCase #-}
+-- | Matching against a list of qualified names.
+qNameIn :: (ArrowXml a) => [QName] -> a XmlTree XmlTree
+qNameIn qNames = (getQName >>> isA (flip elem qNames)) `guards` this
+{-# INLINE qNameIn #-}
 
+-- | Case insensitive variant of 'qNameIn'.
 qNameCaseIn :: (ArrowXml a) => [QName] -> a XmlTree XmlTree
 qNameCaseIn qNames = (getQNameCase >>> isA (flip elem qNames')) `guards` this
   where
     qNames' = map makeQNameCase qNames
 {-# INLINE qNameCaseIn #-}
 
+-- * Stripping elements from the tree
+
+-- | Strip elements from the tree if their name matches against a
+-- unqualified name.
+stripName :: (ArrowXml a) => String -> a XmlTree XmlTree
+stripName n = processTopDown (filterA $ neg (hasName n))
+{-# INLINE stripName #-}
+
+-- this does not work, we need neg or ifA to pass nodes through
+stripName' :: (ArrowXml a) => String -> a XmlTree XmlTree
+stripName' n = processTopDown (filterA (getName >>> isA (/= n)) `guards` this)
+
+-- | Strip elements from the tree if their name matches a member of a
+-- list of unqualified names.
+stripNames :: (ArrowXml a) => [String] -> a XmlTree XmlTree
+stripNames names = processTopDown $ neg $ (nameIn names) `guards` this
+{-# INLINE stripNames #-}
+
+-- | Like 'stripNames' but for qualified names.
+stripQNames :: (ArrowXml a) => [QName] -> a XmlTree XmlTree
+stripQNames qNames = processTopDown $ neg $ (qNameIn qNames) `guards` this
+{-# INLINE stripQNames #-}
+
+-- | Case insensitive variant of 'stripQNames'.
 stripQNamesCase :: (ArrowXml a) => [QName] -> a XmlTree XmlTree
 stripQNamesCase qNames = processTopDown $ neg $ (qNameCaseIn qNames) `guards` this
+{-# INLINE stripQNamesCase #-}
 
 -- * Misc
 
