b127571d0495ff6eca689df280704de7ea42f007
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2018-06-01 11:58:05 +0200

    doc strings

--- src/GraDrAna/App.hs
+++ src/GraDrAna/App.hs
@@ -2,6 +2,12 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
+-- | We define a monad transformer stack for our application to avoid
+-- boilerplate code for passing around the configuration etc.
+--
+-- See http://book.realworldhaskell.org/read/monad-transformers.html
+-- for an introduction on this pattern.
+
 module GraDrAna.App where
 
 import Control.Monad
@@ -12,17 +18,33 @@ import Text.XML.HXT.DOM.TypeDefs (c_err)
 
 import GraDrAna.TypeDefs
 
-type AppConfig = MonadReader Config
-
+-- | The monad transformer stack for the application.
 newtype App a = App {
   runA :: ReaderT Config IO a
   } deriving (Monad, Functor, Applicative, MonadIO, MonadReader Config)
 
+-- | We definitively do not want to allow side effects everywhere. So
+-- we can use 'AppConfig' instead of 'App', to make the config
+-- available but regain purity and make GHC fail on IO.
+--
+-- USAGE:
+--
+-- funWithoutSideEffect :: AppConfig m => Argument -> m ()
+type AppConfig = MonadReader Config
 
+-- | Run an application in the 'App' monad transformer stack.
 runGraDrAnaApp :: App a -> Config -> IO a
 runGraDrAnaApp app cfg = runReaderT (runA app) cfg
 
-
+-- | An application composed of functions given in the configuration.
+--
+-- USAGE:
+--
+-- main :: IO ()
+-- main = runGraDrAnaApp configuredApp def
+--
+-- 'def' from 'Data.Default.Class' in this context returns a default
+-- configuration and can be modified using lenses.
 configuredApp :: App ()
 configuredApp = do
   parsePlay <- asks _cfg_parsePlay
@@ -43,7 +65,7 @@ configuredApp = do
                       else ExitSuccess
                     )
 
-
+-- | A record for the configuration of a GraDrAna application.
 data Config =
   Config
   { _cfg_inFile :: Maybe FilePath -- ^ Nothing means stdin
